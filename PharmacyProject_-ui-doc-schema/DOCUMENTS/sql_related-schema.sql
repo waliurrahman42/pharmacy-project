d4f198bff1ce433ee4a97f0ab1cfd822a363bf9d
Waliur <waliurrahman42@gmail.com>
2022-04-19 16:25:38 +0530

    ui

---
+++ PharmacyProject_-ui-doc-schema/DOCUMENTS/sql_related-schema.sql
@@ -0,0 +1,131 @@
+$.Admin(G3)
+    AdminId             nvarchar(20) (PK),
+    AdminName           nvarchar(20),
+    AdminPassword       nvarchar(100) NOT NULL , 
+
+1. UserAccount(Customer)
+    UserId          nvarchar(20) NOT NULL (PK ) auto,
+    FullName        nvarchar (100) NOT NULL ,
+    EmailID         nvarchar (50) NOT NULL ,
+    ContactNo       nvarchar (50) NULL ,
+    Password        nvarchar (100) NOT NULL ,
+    Status          bit NOT NULL (default active) 
+
+2. UserAddress(Customer)
+	UserAddress_Id     bigint NOT NULL , (PK ) auto
+    Name               nvarchar (100) NOT NULL ,
+    Mobile_no          nvarchar (50) NULL ,
+    PinCode            int(6) NOT NULL
+    Area               nvarchar (300)NOT NULL
+    City               nvarchar (50)NOT NULL
+    State              nvarchar (50)NOT NULL
+    Landmark           nvarchar (50)
+    UserAccount        nvarchar(20)(FK ,UserAccount(UserId))
+	
+
+3.SellerAccount
+    SellerId        nvarchar(20) NOT NULL (FK)
+    EmailId         nvarchar (50) NOT NULL,
+    Password        nvarchar (100) NOT NULL,
+    Status          bit NOT NULL  (default deactive) 
+
+4.SellerInfo
+    SellerInfo_Id      bigint NOT NULL,
+    ShopName           nvarchar (100) NOT NULL ,
+    ShopRegistrationNo bigint NOT NULL,
+    ShopContactNo      nvarchar (50) NULL ,
+    ShopAddress        nvarchar (100) NOT NULL ,
+    ShopAreaPincode    bigint NOT NULL,
+    SellerAccount      nvarchar(20)(FK ,SellerAccount(SellerId))
+
+5. ProductMaster(Medicine)
+    ProductId       bigint NOT NULL (PK)
+    ProductName     nvarchar(100) NOT NULL ,
+    PurchasePrice 	float NOT NULL,
+    MRP			    float NOT NULL,
+    OurPrice 		float NOT NULL,
+    BrandId         int   NOT NULL,(FK, Brand(BrandId))
+    CategoryId 		bigint NOT NULL,(FK CategoryMaster(CMId) )
+    Composition Id	bigint NOT NULL,(FK, ProductType(TypeId))
+    Quantity		bigint NOT NULL,
+    Description 	nvarchar (500) NULL,
+    Image1 			nvarchar (50) NULL,
+    Image2			nvarchar (50) NULL,
+    ValuePoint 		bigint NOT NULL
+
+6. Brand
+    BrandId   int  (PK)
+    ManufacturerName varchar(100) Not Null
+    
+
+6. ProductType
+    TypeId			 bigint NOT NULL ,(PK)
+    TypeName 		 nvarchar (50) NOT NULL ,
+    Description		 nvarchar (500) NULL ,
+    Active			 bit NOT NULL,
+
+
+
+8. Wishlist
+    WishListid 		bigint NOT NULL ,
+    CustomerUserId	nvarchar(20) NULL ,(FK, UserAccount(UserId))
+    ProductId 		bigint NULL ,(FK, ProductMaster(ProductId))
+    InsertDate 		datetime NULL
+
+9. ShopingCart
+    ShopingId		bigint NOT NULL ,(PK)
+    CustomerUserId 	nvarchar(20) NULL ,(FK, , UserAccount(UserId))
+    Date 			datetime NOT NULL ,
+    ProductId 		bigint NOT NULL ,
+    Quantity		bigint NOT NULL ,
+    Price 			float NOT NULL ,
+    Active 			bit NOT NULL
+
+10.OrderMaster
+    OrderId 		    bigint NOT NULL ,(PK)
+    CustomerUserId 	    nvarchar(20) NOT NULL ,(FK, , UserAccount(UserId))
+    OrderDate 		    datetime NOT NULL ,
+    OrderAmount 	    float NOT NULL ,
+    Discount 		    float NULL ,
+    NetAmount           float NOT NULL ,
+    ShippingDate 	    datetime NOT NULL ,
+    ShippingAddressID   bigint NOT NULL ,(FK, , UserAddress( UserAddress_Id))
+    ShippingSellerID    bigint NOT NULL ,(FK, , SellerAccount( SellerId))
+    StatusID 		    tinyint NULL ,
+    Active 			    bit NOT NULL
+
+11. OrderDetails
+    OrderId 		bigint NOT NULL ,(FK)
+    ProductId 		bigint NOT NULL , ,(FK, ProductMaster(ProductId)
+    ProductPrice 	float NOT NULL ,
+    Quantity 		bigint NOT NULL ,
+    Discount 		float NULL ,
+    Active 			bit NOT NULL
+
+12. OrderStatus
+    StatusId 		bigint NOT NULL ,(PK)
+    StatusType 		nvarchar (50) NOT NULL ,
+    Description		nvarchar (500) NULL ,
+    Active 			bit NOT NULL
+
+-- 13. app Review  
+--     ReviewId 		bigint NOT NULL ,(PK)
+--     UserId 			nvarchar(20) NOT NULL , (FK, , UserAccount(UserId))
+--     ReviewText 		nvarchar (500) NULL ,
+--     Active 			bit NOT NULL
+
+
+
+15. FAQ
+    FAQ_Id 			int (PK)
+    FAQ_Que 		nvarchar(200)
+    FAQ_Ans 		nvarchar(2000)
+    Active 			bit
+
+16. Query (helpdesk)
+    QueryID 		int (PK)
+    First_Name 		nvarchar(50)
+    Last_name 		nvarchar(50)
+    Email_id 		nvarchar(50)
+    Subject 		nvarchar(100)
+    QueryText 		nvarchar(MAX)
---
+++ PharmacyProject_-ui-doc-schema/myjScript.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+
+  //   $(window).resize(function () {
+  //       if ($(window).width() < 700) {
+  //           $(".nav-bgcolor").removeClass('nav-bgcolor')
+  //       }
+  //       else{
+  //         $(".navbar").addClass('nav-bgcolor')
+  //       } 
+  // })
+
+
+  //for nav on mobile view
+
+  var fixHeight = function () {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
+
+  $(window).resize(function () {
+    fixHeight();
+  });
+
+  $('.navbar .navbar-toggler').on('click', function () {
+    fixHeight();
+  });
+
+
+  $('.navbar-toggler,.overlay,.btnclsnav').on('click', function () {
+    $('.mobileMenu,.overlay,.btnclsnav').toggleClass('open');
+  });
+
+
+
+  //for banner portion
+  $('.carousel').carousel({
+    interval: 3000,
+    keyboard: true,
+    pause: 'hover',
+    wrap: true
+  });
+
+  $('#slider4').on('slide.bs.carousel', function () {
+    console.log('SLIDE!');
+  });
+
+  $('#slider4').on('slid.bs.carousel', function () {
+    console.log('SLID!');
+  });
+
+
+
+  //current year for copyright
+  document.getElementById("footeryear").innerHTML = new Date().getFullYear();
+});
+
+//usersignup page 
+function checkFullName() {
+  var name = document.getElementById("FullName").value;
+  var alphExp = /^[a-zA-Z]+$/;
+  if ((name.length < 4) && (name.match(alphExp))) {
+    document.getElementById("fullname-error").innerText = "Name must have more than 4 chars and not numric value";
+  } else {
+    document.getElementById("fullname-error").innerText = "";
+  }
+}
+//usersignup page
+function checkContactNo() {
+  var contactno = document.getElementById("ContactNumber").value;
+  var contact_regex = /^[0-9]{10}$/
+  if (!contactno.match(contact_regex))
+ {
+    document.getElementById("ContactNumber-error").innerText = "Contact number Should be 10 digits";
+    //document.getElementsByClassName("").style.color ="red";
+  } else {
+    document.getElementById("ContactNumber-error").innerText = "";
+  }
+}
+
+//usersignup page
+function checkPasswordMatch() {
+  var Password = document.getElementById("Password").value;
+  var RePassword = document.getElementById("RePassword").value;
+  //var alphExp = /^[0-9]+$/;
+  if (Password   != RePassword ) {
+    document.getElementById("RePassword-error").innerText = "Password not matched";
+  } else {
+    document.getElementById("RePassword-error").innerText = "";
+  }
+}
+
+
+// function checkFullName() {
+//   var fullname = $('#FullName').val();
+//   if (fullname.length < 5) {
+//     $('#fullname-error').text = "Fullname length must be greater than 10";
+//   }
+// }
